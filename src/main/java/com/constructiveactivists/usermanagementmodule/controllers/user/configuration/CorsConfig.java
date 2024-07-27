c8de4116f7ef91d00717ba7f770b62e29e5abb63
Daniel-byte628 <danigutierritos2817@gmail.com>
2024-07-26 19:30:04 -0500

    Cross origin configuration

---
+++ src/main/java/com/constructiveactivists/usermanagementmodule/controllers/user/configuration/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.constructiveactivists.usermanagementmodule.controllers.user.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("*");
+    }
+}
