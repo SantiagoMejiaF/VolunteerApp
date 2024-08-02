690cfe32139d9d1251f55553ebc4233c9f50d739
SantiagoMejiaF <santiagomejiaf@javeriana.edu.co>
2024-08-02 08:45:32 -0500

    Ajuste en el cross origin

--- src/main/java/com/constructiveactivists/usermanagementmodule/controllers/user/configuration/CorsConfig.java
+++ src/main/java/com/constructiveactivists/usermanagementmodule/controllers/user/configuration/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.constructiveactivists.usermanagementmodule.controllers.user.configuration;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:4200", "https://volunteer-app.online")
+                .allowedOriginPatterns("http://localhost:4200", "https://www.volunteer-app.online")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .allowedHeaders("*");
