e1670018cdf586ddc9123ff3cbcdb18700d39386
MartinCHz <martin.chen@itus.com.co>
2024-08-09 23:42:11 -0500

    Feat: changed urls

--- src/app/services/oauth.service.ts
+++ src/app/services/oauth.service.ts
@@ -15,14 +15,14 @@ export class OauthService {
   constructor(private httpClient: HttpClient) { }
 
   public google(tokenDto: TokenDto): Observable<any> {
-    return this.httpClient.post<any>(`${environment.apiUrl}/users/google`, tokenDto, cabecera);
+    return this.httpClient.post<any>(`${environment.apiUrl}/authentications/google`, tokenDto, cabecera);
   }
 
   public facebook(tokenDto: TokenDto): Observable<any> {
-    return this.httpClient.post<any>(`${environment.apiUrl}/users/facebook`, tokenDto, cabecera);
+    return this.httpClient.post<any>(`${environment.apiUrl}/authentications/facebook`, tokenDto, cabecera);
   }
 
   public apple(tokenDto: TokenDto): Observable<any> {
-    return this.httpClient.post<any>(`${environment.apiUrl}/users/apple`, tokenDto, cabecera);
+    return this.httpClient.post<any>(`${environment.apiUrl}/authentications/apple`, tokenDto, cabecera);
   }
 }
