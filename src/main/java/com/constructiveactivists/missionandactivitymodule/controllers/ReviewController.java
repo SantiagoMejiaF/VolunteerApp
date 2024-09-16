f31e10d528c1339acbafc53f285ecf31cb823c7f
Daniel-byte628 <danigutierritos2817@gmail.com>
2024-09-16 17:39:41 -0500

    Bug imagen corregido

--- src/main/java/com/constructiveactivists/missionandactivitymodule/controllers/ReviewController.java
+++ src/main/java/com/constructiveactivists/missionandactivitymodule/controllers/ReviewController.java
@@ -14,8 +14,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController
 @AllArgsConstructor
 @RequestMapping("${request-mapping.controller.review}")
 public class ReviewController implements ReviewAPI {
