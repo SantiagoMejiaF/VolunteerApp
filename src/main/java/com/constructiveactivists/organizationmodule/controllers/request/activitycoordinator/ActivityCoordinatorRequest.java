3f797e889aa037c6a53f617046e763efc31e08b5
Daniel-byte628 <danigutierritos2817@gmail.com>
2024-09-30 10:31:44 -0500

    cambios de agregar coordinador

--- src/main/java/com/constructiveactivists/organizationmodule/controllers/request/activitycoordinator/ActivityCoordinatorRequest.java
+++ src/main/java/com/constructiveactivists/organizationmodule/controllers/request/activitycoordinator/ActivityCoordinatorRequest.java
@@ -1,6 +1,5 @@
 package com.constructiveactivists.organizationmodule.controllers.request.activitycoordinator;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -20,30 +19,19 @@ public class ActivityCoordinatorRequest {
     @Schema(description = "ID de la organización asociada al coordinador de actividad", example = "456")
     private Integer organizationId;
 
-    @NotBlank(message = "El nombre del coordinador de actividad es obligatorio.")
-    @Schema(description = "Nombre del coordinador de actividad", example = "María")
-    private String nameActivityCoordinator;
-
-    @NotBlank(message = "El apellido del coordinador de actividad es obligatorio.")
-    @Schema(description = "Apellido del coordinador de actividad", example = "Gómez")
-    private String lastNameActivityCoordinator;
+    @NotNull(message = "El UserId del coordinador de actividad es obligatorio.")
+    @Schema(description = "UserId del coordinador de actividad", example = "123")
+    private Integer userId;
 
     @NotBlank(message = "La cédula es obligatoria.")
     @Pattern(regexp = "^\\d{10}$", message = "La cédula debe tener exactamente 10 dígitos.")
     @Schema(description = "Cédula del coordinador de actividad", example = "0987654321")
     private String identificationCard;
 
-    @NotBlank(message = "El correo electrónico es obligatorio.")
-    @Email(message = "El formato del correo electrónico es inválido.")
-    @Schema(description = "Correo electrónico del coordinador de actividad", example = "maria.gomez@example.com")
-    private String emailActivityCoordinator;
-
     @NotBlank(message = "El número de celular es obligatorio.")
     @Pattern(regexp = "^\\d{10}$", message = "El número de celular debe tener exactamente 10 dígitos.")
     @Schema(description = "Número de celular del coordinador de actividad", example = "3109876543")
     private String phoneActivityCoordinator;
 
-    @Schema(description = "Imagen del coordinador de actividad", example = "https://example.com/image.jpg")
-    private String image;
 }
 
