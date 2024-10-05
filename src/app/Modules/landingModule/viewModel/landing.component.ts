3e927efe30fc19c45d07c1884f1150a42d6e56f4
valenh20023008 <valenh20023008@gmail.com>
2024-10-05 18:56:23 -0500

    Feat:Admin

--- src/app/Modules/landingModule/viewModel/landing.component.ts
+++ src/app/Modules/landingModule/viewModel/landing.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
@@ -6,6 +8,24 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
   styleUrls: ['../styles/landing.component.css']
 })
 export class LandingComponent  {
-//Prueba
+  constructor(
+    private route: ActivatedRoute,
+    private viewportScroller: ViewportScroller,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // Detectar el fragmento al cargar la página
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        this.scrollToFragment(fragment);
+      }
+    });
+  }
+
+  scrollToFragment(fragment: string): void {
+    // Usar ViewportScroller para hacer scroll a la sección correspondiente
+    this.viewportScroller.scrollToAnchor(fragment);
+  }
   
 }
